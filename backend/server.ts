d7b385bbc22b1bf42f924d801d19817ce2c8766a
adiel23113 <adielbhuiya@gmail.com>
2026-08-02 20:42:16 +0600

    sigiterm shutdown

--- backend/server.ts
+++ backend/server.ts
@@ -31,5 +31,17 @@ const shutdown = async (signal: string): Promise<void> => {
         }
     }
 
+process.on('SIGTERM', () => shutdown('SIGTERM'))
+process.on('SIGINT', () => shutdown('SIGINT'))
+
+process.once('unhandledRejection', (reason: unknown) => {
+    logger.error({err: reason}, 'unhandled rejection shutdown')
+    shutdown('unhandledRejection')
+})
+
+process.once('uncaughtException', (err: Error) => {
+    logger.fatal({err}, 'uncaught exception shutdown')
+    shutdown('uncaughtException')
+})
 
 
