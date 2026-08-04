26a2166ee84db684086fffea4dda0dcc7d8888f3
adiel23113 <adielbhuiya@gmail.com>
2026-08-04 11:02:35 +0600

    monngoss ar connection options

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,13 +1,38 @@
-import mongoose from "mongoose";
+import mongoose,{type ConnectOptions} from 'mongoose';
 import { logger } from "../utils/logger.js";
 import { env } from "./env.js";
 
-export const connectDb = async (): Promise<void> => {
-    if (mongoose.connection.readyState === 1) return;
 
-    await mongoose.connect(env.MONGODB_URI);
-    logger.info('mongodb connected');
-};
+
+mongoose.connection.on('error', (err) => {
+    logger.error({err},'mongodb connection error')
+})
+mongoose.connection.on('disconnected', () => {
+    logger.warn('mongodb connection disconnected')
+})
+mongoose.connection.on('reConnection',() => {
+    logger.info('mongodb reConnected')
+})
+
+const isProduction = process.env.NODE_ENV === 'production'
+const connection_options:ConnectOptions = {
+    maxPoolSize: isProduction? 100:10,
+    minPoolSize: isProduction? 10:2,
+    serverSelectionTimeoutMS: 10_100,
+    socketTimeoutMS: 45_100,
+    heartbeatFrequencyMS: 10_000,
+    retryWrites: true,
+    compressors :['snappy','zstd']
+}
+
+export const connectDb = async (): Promise<void> => {
+    if (mongoose.connection.readyState === 1) return
+    const connectionDb = await mongoose.connect(env.MONGODB_URI,connection_options)
+    logger.info({
+        host: connectionDb.connection.host,
+        name: connectionDb.connection.name
+    }, 'mongodb connected')
+}
 
 export const disconnectDb = async (): Promise<void> => {
     if (mongoose.connection.readyState === 0) return;
